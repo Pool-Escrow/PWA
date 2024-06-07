2436810aac6ddd1d129beb38ee07a1e4cc41fca0
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-07 17:58:15 +0200

    chore: Update Providers component with new PrivyProvider configuration decided during recent conversations

--- components/providers.tsx
+++ components/providers.tsx
@@ -11,49 +11,29 @@ import { WagmiProvider } from 'wagmi'
 import { Toaster } from '@/components/ui/toaster'
 
 export default function Providers({ children }: React.PropsWithChildren) {
-	// const configureChainsConfig = createConfig({
-	// 	chains: [mainnet, goerli, foundry],
-	// 	transports: {
-	// 		[foundry.id]: http(),
-	// 		[sepolia.id]: http(),
-	// 	},
-	// })
-
 	const queryClient = new QueryClient()
 
 	return (
 		<PrivyProvider
 			appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
 			config={{
-				loginMethods: [
-					// 'google',
-					'wallet',
-					// 'farcaster',
-				],
+				loginMethods: ['email', 'wallet', 'farcaster'],
 				appearance: {
 					theme: 'light',
 					accentColor: '#676FFF',
 					logo: '/images/pool.png',
-					// showWalletLoginFirst: false,
+					showWalletLoginFirst: false,
 				},
 				embeddedWallets: {
-					createOnLogin: 'off',
-					// createOnLogin: 'users-without-wallets',
+					createOnLogin: 'users-without-wallets',
+					noPromptOnSignature: true,
 					priceDisplay: {
 						primary: 'native-token',
 						secondary: null,
 					},
 				},
-
 				defaultChain: chain,
-				supportedChains: [
-					base,
-					baseSepolia,
-					// baseGoerli,
-					// mainnet,
-					// goerli,
-					// chain,
-				],
+				supportedChains: [base, baseSepolia],
 				legal: {
 					privacyPolicyUrl: '/privacy',
 					termsAndConditionsUrl: '/terms',
