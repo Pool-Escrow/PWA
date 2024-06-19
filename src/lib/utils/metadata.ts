c67bb522ee15739fc19683cc1546854077eda61f
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-19 19:18:12 +0200

    refactor: update metadata.ts for consistent code formatting

--- src/lib/utils/metadata.ts
+++ src/lib/utils/metadata.ts
@@ -1,19 +1,19 @@
-import { Metadata, Viewport } from 'next'
+import type { Metadata, Viewport } from 'next'
 
 export const viewport: Viewport = {
-	maximumScale: 5,
-	minimumScale: 1,
-	viewportFit: 'cover',
-	themeColor: [
-		{ media: '(prefers-color-scheme: dark)', color: '#f4f4f5' },
-		{ media: '(prefers-color-scheme: light)', color: '#18181b' },
-	],
+    maximumScale: 5,
+    minimumScale: 1,
+    viewportFit: 'cover',
+    themeColor: [
+        { media: '(prefers-color-scheme: dark)', color: '#fff' },
+        { media: '(prefers-color-scheme: light)', color: '#18181b' },
+    ],
 }
 
 export const metadata: Metadata = {
-	icons: {
-		icon: '/images/favicon.png',
-		apple: '/images/icon-maskable-512.png',
-	},
-	manifest: '/manifest.json',
+    icons: {
+        icon: '/images/favicon.png',
+        apple: '/images/icon-maskable-512.png',
+    },
+    manifest: '/manifest.json',
 }
