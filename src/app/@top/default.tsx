041fdff1bf377e2418f210c2495b67bc896b4089
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-19 19:26:11 +0200

    refactor: optimize TopBarLayout component rendering

--- src/app/@top/default.tsx
+++ src/app/@top/default.tsx
@@ -3,16 +3,16 @@
 import { getTopBarElements } from '@/components/shared/layout/top-bar/top-bar.config'
 import { usePathname } from 'next/navigation'
 
-export default function TopBarLayout() {
-	const pathname = usePathname()
-	const { left, center, right } = getTopBarElements(pathname)
-	return (
-		<header className='fixed left-0 top-0 w-full z-10 bg-white min-h-top-bar pb-5 pt-safe-or-5 px-safe-or-6'>
-			<nav className='flex-center h-full max-w-screen-md *:items-center *:flex *:flex-1'>
-				<div className='justify-start'>{left}</div>
-				<div className='justify-center'>{center}</div>
-				<div className='justify-end'>{right}</div>
-			</nav>
-		</header>
-	)
+export default function TopBarLayout(): JSX.Element {
+    const pathname = usePathname()
+    const { left, center, right } = getTopBarElements(pathname)
+    return (
+        <header className='fixed left-0 top-0 z-10 h-top-bar min-w-full '>
+            <nav className='flex-center h-full max-w-screen-md bg-white pb-5 pt-safe-or-5 px-safe-or-6 *:flex *:flex-1 *:items-center'>
+                <div className='justify-start'>{left}</div>
+                <div className='justify-center'>{center}</div>
+                <div className='justify-end'>{right}</div>
+            </nav>
+        </header>
+    )
 }
