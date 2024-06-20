1a1326462ae8201c094c28c867777baee71bb9b6
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-20 07:12:23 +0200

    refactor: Update next.config.mjs to improve service worker configuration and enable bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,33 @@
 // @ts-check
 import bundleAnalyzer from '@next/bundle-analyzer'
 import withSerwistInit from '@serwist/next'
-
-const inProduction = process.env.NODE_ENV === 'production'
+import { inProduction } from './src/lib/utils/environment.mjs'
 
 const withSerwist = withSerwistInit({
-	swSrc: 'src/sw.ts',
-	swDest: 'public/sw.js',
-	disable: !inProduction,
+    swSrc: 'src/lib/utils/sw.ts',
+    swDest: 'public/sw.js',
+    disable: !inProduction,
 })
 
 const withBundleAnalyzer = bundleAnalyzer({
-	enabled: process.env.ANALYZE === 'true',
+    enabled: process.env.ANALYZE === 'true',
 })
 
 /** @type {import('next').NextConfig} */
 export default withBundleAnalyzer(
-	withSerwist({
-		eslint: { ignoreDuringBuilds: true },
-		compiler: { removeConsole: inProduction },
-		experimental: { typedRoutes: !inProduction },
-		reactStrictMode: !inProduction,
-		images: {
-			remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
-		},
-		redirects: async () => [
-			{ source: '/', destination: '/pools', permanent: true },
-		],
-	}),
+    withSerwist({
+        eslint: { ignoreDuringBuilds: true },
+        compiler: { removeConsole: inProduction },
+        experimental: {
+            typedRoutes: !inProduction,
+            // turbo: {
+            // useSwcCss: true,
+            // },
+        },
+        reactStrictMode: !inProduction,
+        images: {
+            remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
+        },
+        redirects: async () => Promise.resolve([{ source: '/', destination: '/pools', permanent: true }]),
+    }),
 )
