8a13825b148bbc830ccf33d6c5c82637fb0ff973
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-20 07:10:31 +0200

    refactor: Update manifest.json for PWA support

--- src/lib/utils/metadata.ts
+++ src/lib/utils/metadata.ts
@@ -10,10 +10,47 @@ export const viewport: Viewport = {
     ],
 }
 
+const APP_NAME = 'PWA App'
+const APP_DEFAULT_TITLE = 'My Awesome PWA App'
+const APP_TITLE_TEMPLATE = '%s - PWA App'
+const APP_DESCRIPTION = 'Best PWA app in the world!'
+
 export const metadata: Metadata = {
     icons: {
         icon: '/images/favicon.png',
         apple: '/images/icon-maskable-512.png',
     },
     manifest: '/manifest.json',
+    applicationName: APP_NAME,
+    title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'default',
+        title: APP_DEFAULT_TITLE,
+        // startUpImage: [],
+    },
+    formatDetection: {
+        telephone: false,
+    },
+    openGraph: {
+        type: 'website',
+        siteName: APP_NAME,
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: 'summary',
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
 }
