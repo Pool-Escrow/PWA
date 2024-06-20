2d75a4f71235d61475ae1bcc89f8ba6a600f4ba5
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-20 07:22:00 +0200

    styles: fix bottom bar sizing and positioning

--- src/app/@bottom/default.tsx
+++ src/app/@bottom/default.tsx
@@ -1,29 +1,29 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
+import { toast } from 'sonner'
 
 export default function BottomBar() {
-	return (
-		<footer className='fixed flex-center bottom-0 left-0 min-h-bottom-bar rounded-t-3xl bg-neutral-100 pt-4 pb-safe-or-4 px-3 shadow-black/25 shadow backdrop-blur-[32.10px]'>
-			<Button className='w-full h-full bg-cta text-center text-white text-base font-semibold leading-normal rounded-[2rem] p-3'>
-				Register for $25 USDC
-			</Button>
-			{/* <Button className='bottom-6 w-[345px] rounded-[32px]  py-[11px] shadow-[inset_0px_1.75px_0px_0px_rgba(255,255,255,0.25)]'>
-				Create Pool
-			</Button> */}
-		</footer>
-	)
-}
+    const handleClick = () => {
+        // reload the page
+        toast.message('Creating Pool', {
+            description: 'Please wait...',
+            duration: 1450,
+        })
+        setTimeout(() => {
+            window.location.reload()
+        }, 1500)
+    }
 
-// export default function BottomBar() {
-// 	return (
-// 		<footer className='fixed flex-center bottom-0 bg-white border-t border-gray-200 h-bottom-bar pb-safe-offset-4 px-safe-offset-3'>
-// 			<nav className='w-[369px] h-[46px] px-[97px] py-[11px] bg-cta rounded-[32px] shadow-inner justify-center items-center inline-flex'>
-// 				<div className='text-center text-white text-base font-semibold leading-normal'>
-// 					Register for $25 USDC
-// 				</div>
-// 			</nav>
-// 			{/* <Button className='bottom-6 w-[345px] rounded-[32px]  py-[11px] shadow-[inset_0px_1.75px_0px_0px_rgba(255,255,255,0.25)]'>
-// 				Create Pool
-// 			</Button> */}
-// 		</footer>
-// 	)
-// }
+    return (
+        <footer className='flex-center fixed bottom-0 left-0 min-w-full'>
+            <nav className='flex-center my-auto h-bottom-bar max-w-screen-md rounded-t-3xl bg-neutral-100/50 shadow shadow-black/25 backdrop-blur-[32.10px] px-safe-or-6'>
+                <Button
+                    className='mb-3 h-[46px] w-full rounded-[2rem] bg-cta px-6 py-[11px] text-center text-base font-semibold leading-normal text-white shadow-button active:shadow-button-push'
+                    onClick={handleClick}>
+                    Create Pool
+                </Button>
+            </nav>
+        </footer>
+    )
+}
