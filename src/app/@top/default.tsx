7b743590ee3fe1e2b19d75d73f32c0bb1990c38c
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-20 07:17:30 +0200

    refactor: Remove unnecessary whitespace in TopBarLayout component

--- src/app/@top/default.tsx
+++ src/app/@top/default.tsx
@@ -7,7 +7,7 @@ export default function TopBarLayout(): JSX.Element {
     const pathname = usePathname()
     const { left, center, right } = getTopBarElements(pathname)
     return (
-        <header className='fixed left-0 top-0 z-10 h-top-bar min-w-full '>
+        <header className='fixed left-0 top-0 z-10 h-top-bar min-w-full'>
             <nav className='flex-center h-full max-w-screen-md bg-white pb-5 pt-safe-or-5 px-safe-or-6 *:flex *:flex-1 *:items-center'>
                 <div className='justify-start'>{left}</div>
                 <div className='justify-center'>{center}</div>
