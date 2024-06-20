e2dbe88b0e244f01a27ea3b45bfa3c557e08df33
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-20 07:15:40 +0200

    chore: Update postcss.config.mjs to include autoprefixer plugin and conditional cssnano plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 
+import { inProduction } from './src/lib/utils/environment.mjs'
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: {
         tailwindcss: {},
+        autoprefixer: {},
+        // minify and purges unused css
+        ...(inProduction ? { cssnano: {} } : {}),
     },
 }
 
