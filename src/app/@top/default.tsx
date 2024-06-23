133066fab29180781957d1f067e9b87075ed1456
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-23 03:37:13 +0200

    refactor: improve top-bar implementation

--- src/app/@top/default.tsx
+++ src/app/@top/default.tsx
@@ -1,18 +1,6 @@
-'use client'
+/**
+ * @file src/app/@top/default.tsx
+ * @description parallel route for the default top bar
+ */
 
-import { getTopBarElements } from '@/components/shared/layout/top-bar/top-bar.config'
-import { usePathname } from 'next/navigation'
-
-export default function TopBarLayout(): JSX.Element {
-    const pathname = usePathname()
-    const { left, center, right } = getTopBarElements(pathname)
-    return (
-        <header className='fixed left-0 top-0 z-10 h-top-bar min-w-full'>
-            <nav className='flex-center h-full max-w-screen-md bg-white pb-5 pt-safe-or-5 px-safe-or-6 *:flex *:flex-1 *:items-center'>
-                <div className='justify-start'>{left}</div>
-                <div className='justify-center'>{center}</div>
-                <div className='justify-end'>{right}</div>
-            </nav>
-        </header>
-    )
-}
+export { TopBar as default } from '@/components/top-bar'
