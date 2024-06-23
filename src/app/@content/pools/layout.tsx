cb35ec3f681f399467db4d196b83a86712b1a2f5
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-23 03:56:08 +0200

    chore: Update page layout and navigation in pools module

--- src/app/@content/pools/layout.tsx
+++ src/app/@content/pools/layout.tsx
@@ -1,11 +1,20 @@
-export default function PoolsLayout({
-	yours,
-	upcoming,
-}: LayoutWithSlots<'yours' | 'upcoming'>) {
-	return (
-		<div className='space-y-6'>
-			{yours}
-			{upcoming}
-		</div>
-	)
+'use client'
+
+import { useBottomBarStore } from '@/providers/bottom-bar.provider'
+import { useEffect } from 'react'
+
+export default function PoolsLayout({ yours, upcoming }: LayoutWithSlots<'yours' | 'upcoming'>) {
+    const hideBar = useBottomBarStore(state => state.hideBar)
+
+    useEffect(() => {
+        console.log('PoolsLayout mounted')
+        hideBar()
+    }, [])
+
+    return (
+        <div className='flex min-h-dvh scroll-py-6 flex-col gap-6'>
+            {yours}
+            {upcoming}
+        </div>
+    )
 }
