37f956155798f5d919c17632b8e14a12d775bc49
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-23 03:29:13 +0200

    chore: configure tsconfig for test

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import bundleAnalyzer from '@next/bundle-analyzer'
 import withSerwistInit from '@serwist/next'
 import { inProduction } from './src/lib/utils/environment.mjs'
 
+const turboEnabled = process.env.TURBO === 'true'
+
 const withSerwist = withSerwistInit({
     swSrc: 'src/lib/utils/sw.ts',
     swDest: 'public/sw.js',
@@ -17,17 +19,23 @@ const withBundleAnalyzer = bundleAnalyzer({
 export default withBundleAnalyzer(
     withSerwist({
         eslint: { ignoreDuringBuilds: true },
-        compiler: { removeConsole: inProduction },
+        ...(turboEnabled ? {} : { compiler: { removeConsole: inProduction } }),
         experimental: {
-            typedRoutes: !inProduction,
-            // turbo: {
-            // useSwcCss: true,
-            // },
+            typedRoutes: !inProduction && !turboEnabled,
+            ...(turboEnabled ? { turbo: { useSwcCss: true } } : {}),
         },
         reactStrictMode: !inProduction,
         images: {
             remotePatterns: [{ protocol: 'https', hostname: '*.supabase.co' }],
         },
         redirects: async () => Promise.resolve([{ source: '/', destination: '/pools', permanent: true }]),
+        webpack: (config, { dev, isServer }) => {
+            // Exclude *.test.ts(x) files from being compiled by Next.js
+            config.module.rules.push({
+                test: /\.test\.tsx?$/,
+                use: 'ignore-loader',
+            })
+            return config
+        },
     }),
 )
