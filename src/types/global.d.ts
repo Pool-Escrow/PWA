d61e78733b439851520d785f52cf54a6fdad5fe9
eldante <dan.tgw@gmail.com>
2024-06-24 00:40:45 +0800

    Read poolDetails from smart contract. Fixed some type errors.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,15 +1,24 @@
 declare global {
-	type LayoutWithSlots<T extends string> = {
-		[K in T]: React.ReactNode
-	}
+    type LayoutWithSlots<T extends string> = {
+        [K in T]: React.ReactNode
+    }
 
-	interface Pool {
-		id: bigint
-		name: string
-		startTime: Date
-		endTime: Date
-		status: 'live' | 'upcoming' | 'past'
-	}
+    interface Pool {
+        id: bigint
+        name: string
+        startTime: Date
+        endTime: Date
+        status: 'live' | 'upcoming' | 'past'
+    }
+    interface PoolDetails {
+        id: bigint
+        name: string
+        startTime: Date
+        endTime: Date
+        status: 'live' | 'upcoming' | 'past'
+    }
+
+    type HexString = `0x${string}`
 }
 
 export {}
