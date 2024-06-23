72dc2ff6818415f76193f7a645de9ca6834d8485
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-23 09:29:17 +0200

    chore: Refactor usePools hook to use correct provider configuration

--- src/lib/contracts/fetch-pools.ts
+++ src/lib/contracts/fetch-pools.ts
@@ -1,48 +1,61 @@
-import { wagmi } from '@/components/shared/providers/configs'
+import { wagmi } from '@/providers/configs'
+import wagmiConfig from '@/providers/configs/wagmi.config'
 import { poolAbi, poolAddress } from '@/types/contracts'
 import { getPublicClient } from '@wagmi/core'
 
 export const fetchPools = async () => {
-	if (!process.env.NEXT_PUBLIC_INITIAL_BLOCK) {
-		throw new Error('Initial block not set')
-	}
+    if (!process.env.NEXT_PUBLIC_INITIAL_BLOCK) {
+        throw new Error('Initial block not set')
+    }
 
-	const initialBlock = BigInt(process.env.NEXT_PUBLIC_INITIAL_BLOCK)
-	const publicClient = getPublicClient(wagmi.config)
+    const initialBlock = BigInt(process.env.NEXT_PUBLIC_INITIAL_BLOCK)
+    const publicClient = getPublicClient(wagmi.config)
 
-	const poolLogs = await publicClient.getContractEvents({
-		abi: poolAbi,
-		address: poolAddress[publicClient.chain.id],
-		eventName: 'PoolCreated',
-		fromBlock: initialBlock,
-		toBlock: 'latest',
-	})
+    const chainId = wagmiConfig.config.state.chainId as keyof typeof poolAddress
+    if (!(chainId in poolAddress)) {
+        throw new Error(`Invalid chainId: ${chainId}`)
+    }
 
-	const pools = await Promise.all(
-		poolLogs.map(async (log) => {
-			const { poolId } = log.args
-			if (!poolId) return
-			const pool = await publicClient.readContract({
-				abi: poolAbi,
-				address: poolAddress[publicClient.chain.id],
-				functionName: 'getPoolDetail',
-				args: [poolId],
-			})
+    const poolLogs = await publicClient?.getContractEvents({
+        abi: poolAbi,
+        address: poolAddress[chainId],
+        eventName: 'PoolCreated',
+        fromBlock: initialBlock,
+        toBlock: 'latest',
+    })
 
-			const startTime = new Date(pool.timeStart * 1000)
-			const endTime = new Date(pool.timeEnd * 1000)
-			const now = new Date()
-			const status: 'upcoming' | 'live' | 'past' =
-				startTime > now ? 'upcoming' : endTime > now ? 'live' : 'past'
+    if (!poolLogs) {
+        throw new Error('No pool logs found')
+    }
 
-			return {
-				id: poolId,
-				name: pool.poolName,
-				startTime,
-				endTime,
-				status,
-			}
-		}),
-	)
-	return pools
+    const pools = await Promise.all(
+        poolLogs.map(async log => {
+            const { poolId } = log.args
+            if (!poolId) return
+            const pool = await publicClient?.readContract({
+                abi: poolAbi,
+                address: poolAddress[chainId],
+                functionName: 'getPoolDetail',
+                args: [poolId],
+            })
+
+            if (!pool) {
+                throw new Error(`No pool found for id: ${poolId}`)
+            }
+
+            const startTime = new Date(pool.timeStart * 1000)
+            const endTime = new Date(pool.timeEnd * 1000)
+            const now = new Date()
+            const status: 'upcoming' | 'live' | 'past' = startTime > now ? 'upcoming' : endTime > now ? 'live' : 'past'
+
+            return {
+                id: poolId,
+                name: pool.poolName,
+                startTime,
+                endTime,
+                status,
+            }
+        }),
+    )
+    return pools
 }
