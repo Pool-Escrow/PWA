649c23a8ee2aaacc5b564111b53ddc9b0129abfa
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-23 03:36:41 +0200

    refactor: revisit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,26 @@ import { NextResponse } from 'next/server'
 import { getWalletAddress, isAdmin } from './lib/server/auth'
 
 export const config = {
-	matcher: '/admin/:path*',
+    // matcher: '/profile/:path*',
+    matcher: '/developer/:path*',
 }
 
 export async function middleware(request: NextRequest) {
-	console.log('middleware')
-	const privyAuthToken = request.cookies.get('privy-token')?.value
-	if (!privyAuthToken) {
-		return NextResponse.rewrite(new URL('/login', request.url))
-	}
+    console.log('middleware')
+    const privyAuthToken = request.cookies.get('privy-token')?.value
+    if (!privyAuthToken) {
+        // TODO: redirecto to login page instead
+        return NextResponse.rewrite(new URL('/', request.url))
+    }
 
-	const address = await getWalletAddress(privyAuthToken)
+    const address = await getWalletAddress(privyAuthToken)
 
-	if (!(await isAdmin(address))) {
-		NextResponse.rewrite(new URL('/', request.url))
-	}
+    console.log('address', address)
 
-	return NextResponse.next()
+    if (!(await isAdmin(address))) {
+        console.log('not admin')
+        // NextResponse.rewrite(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
 }
