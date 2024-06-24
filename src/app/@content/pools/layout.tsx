86646868236d668cecc3e3b1c718a11bb2d7eae3
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-24 03:34:23 +0200

    fix: prevent hiding bottom bar at home page

--- src/app/@content/pools/layout.tsx
+++ src/app/@content/pools/layout.tsx
@@ -1,16 +1,6 @@
 'use client'
 
-import { useBottomBarStore } from '@/providers/bottom-bar.provider'
-import { useEffect } from 'react'
-
 export default function PoolsLayout({ yours, upcoming }: LayoutWithSlots<'yours' | 'upcoming'>) {
-    const hideBar = useBottomBarStore(state => state.hideBar)
-
-    useEffect(() => {
-        console.log('PoolsLayout mounted')
-        hideBar()
-    }, [])
-
     return (
         <div className='flex min-h-dvh scroll-py-6 flex-col gap-6'>
             {yours}
