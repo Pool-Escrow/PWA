16f128bed4a18ef8927735172d76cffad0725b8d
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-26 03:15:54 +0200

    refactor: Update pool mock data structure

--- src/stores/data/pools.mock.ts
+++ src/stores/data/pools.mock.ts
@@ -1,50 +1,44 @@
-export const poolsMock: () => Pool[] = () => [
+export const poolsMock: () => Partial<PoolFrontend>[] = () => [
     {
-        id: '1',
+        id: 1,
         name: 'Pool 1',
-        status: 'upcoming',
         // it starts in 1 day and ends in 2 days
-        startTime: new Date(Date.now() + 1000 * 60 * 60 * 24),
-        endTime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 2),
+        startDate: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        endDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 2),
     },
     {
-        id: '2',
+        id: 2,
         name: 'Pool 2',
-        status: 'upcoming',
         // it starts in 30 minutes and ends in 1 hour
-        startTime: new Date(Date.now() + 1000 * 60 * 30),
-        endTime: new Date(Date.now() + 1000 * 60 * 60),
+        startDate: new Date(Date.now() + 1000 * 60 * 30),
+        endDate: new Date(Date.now() + 1000 * 60 * 60),
     },
     {
-        id: '3',
+        id: 3,
         name: 'Pool 3',
-        status: 'past',
         // it started 1 week ago and ended 40 seconds ago
-        startTime: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7),
-        endTime: new Date(Date.now() - 1000 * 40),
+        startDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7),
+        endDate: new Date(Date.now() - 1000 * 40),
     },
     {
-        id: '4',
+        id: 4,
         name: 'Pool 4',
-        status: 'past',
         // it started 2 days ago and ended 1 day ago
-        startTime: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
-        endTime: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        startDate: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
+        endDate: new Date(Date.now() - 1000 * 60 * 60 * 24),
     },
     {
-        id: '5',
+        id: 5,
         name: 'Pool 5',
-        status: 'live',
         // it started 1 day ago and ends in 1 day
-        startTime: new Date(Date.now() - 1000 * 60 * 60 * 24),
-        endTime: new Date(Date.now() + 1000 * 60 * 60 * 24),
+        startDate: new Date(Date.now() - 1000 * 60 * 60 * 24),
+        endDate: new Date(Date.now() + 1000 * 60 * 60 * 24),
     },
     {
-        id: '6',
+        id: 6,
         name: 'Pool 6',
-        status: 'live',
         // it started 2 hours ago and ends in 3 hours
-        startTime: new Date(Date.now() - 1000 * 60 * 60 * 2),
-        endTime: new Date(Date.now() + 1000 * 60 * 60 * 3),
+        startDate: new Date(Date.now() - 1000 * 60 * 60 * 2),
+        endDate: new Date(Date.now() + 1000 * 60 * 60 * 3),
     },
 ]
