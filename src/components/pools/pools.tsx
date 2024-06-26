ab5e1b81bb529990027d0913b493ae867efa28d0
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-26 03:18:53 +0200

    refactor: Update pools.tsx to export pool-list component

--- src/components/pools/pools.tsx
+++ src/components/pools/pools.tsx
@@ -1,20 +1,3 @@
 // src/components/pools/pools.tsx
 
-import PoolList from '@/components/pools/pool-list/pool-list'
-import { fetchPools } from '@/lib/contracts/fetch-pools'
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
-
-export default async function PoolsPage() {
-    const queryClient = new QueryClient()
-
-    await queryClient.prefetchQuery({
-        queryKey: ['pools'],
-        queryFn: fetchPools,
-    })
-
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <PoolList />
-        </HydrationBoundary>
-    )
-}
+export { default } from './pool-list/pool-list'
