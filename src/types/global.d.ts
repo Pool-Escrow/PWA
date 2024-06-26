af99a85a4439f1d12ba515862fa96582799e17eb
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-26 03:16:12 +0200

    refactor: Update pool data structure and add missing fields

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,14 +1,34 @@
+import { getAbiItem, parseAbiItem } from 'viem'
+import { poolAbi, poolAddress } from './contracts'
+
 declare global {
     type LayoutWithSlots<T extends string> = {
         [K in T]: React.ReactNode
     }
 
-    interface Pool {
-        id: string
+    type ChainId = keyof typeof poolAddress
+
+    type PoolFromContract = ContractFunctionReturnType<typeof poolAbi, 'view', 'getPoolDetail'> & {
+        id: number
+        tokenAddress: Address
+    }
+
+    type PoolFromDb = Omit<
+        Database['public']['Tables']['pools']['Row'],
+        'internal_id' | 'createdAt' | 'updatedAt' | 'status'
+    >
+
+    type PoolStatus = 'live' | 'upcoming' | 'past'
+
+    type PoolFrontend = {
+        id: number
+        contract_id: number
         name: string
-        startTime: Date
-        endTime: Date
-        status: 'live' | 'upcoming' | 'past'
+        tokenAddress: Address
+        description: string
+        bannerImage: string | null
+        startDate: Date
+        endDate: Date
     }
 }
 
