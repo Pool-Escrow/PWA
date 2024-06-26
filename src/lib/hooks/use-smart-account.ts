8282342a7e90cee5a5a3f56327cb06e5ff09c5b8
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-26 03:17:26 +0200

    refactor: Update useSmartAccount to create or update user on login

--- src/lib/hooks/use-smart-account.ts
+++ src/lib/hooks/use-smart-account.ts
@@ -4,17 +4,36 @@ import { useLogin, type CallbackError } from '@privy-io/react-auth'
 import { useState } from 'react'
 import { useErrorHandling } from './use-error-handling'
 import { useInitializeAccount } from './use-initialize-account'
+import { createOrUpdateUser } from '@/components/profile/profile.action'
+import { useRouter } from 'next/navigation'
 
 export const useSmartAccount = () => {
+    const router = useRouter()
     const [loading, setLoading] = useState(false)
     const { error, handleError, resetError } = useErrorHandling()
     const initializeAccount = useInitializeAccount(handleError)
 
     const { login } = useLogin({
-        onComplete: (_user, isNewUser) => {
-            if (!isNewUser) return
+        onComplete: async (user, isNewUser) => {
+            if (!user.wallet) {
+                throw new Error('Wallet not found')
+            }
+
+            const result = await createOrUpdateUser(user.id, user.wallet.address)
+
+            if (result.isNewUser) {
+                router.push('/participant/new')
+            } else if (!isNewUser) {
+                // user already exists in privy, but not in our db
+                console.log('User already exists in privy, but not in our db')
+                router.push('/participant/new')
+            } else {
+                router.push('/')
+            }
+
             setLoading(true)
             resetError()
+
             try {
                 void initializeAccount()
             } catch (error) {
