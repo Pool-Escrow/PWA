d7ba08d23459d9d5e6b3655d2ffd1785ac5a9cfa
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-06-29 19:49:38 +0200

    fix: updating API route to match nextjs13+ syntax

--- src/app/api/is_admin/route.tsx
+++ src/app/api/is_admin/route.tsx
@@ -1,23 +1,21 @@
 import 'server-only'
 
 import { getWalletAddress, isAdmin } from '@/lib/server/auth'
-import type { NextApiRequest } from 'next'
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export async function POST(req: NextApiRequest) {
-    // Parse the request body
+export async function POST(request: Request) {
     console.log('is_admin API Hit')
 
-    const privyAuthToken = req.cookies?.['privy-token']
-    // .get('privy-token')?.value
+    const cookieStore = cookies()
+    const privyAuthToken = cookieStore.get('privy-token')?.value
+
     if (!privyAuthToken) {
-        // TODO: redirecto to login page instead
+        // TODO: redirect to login page instead
         return NextResponse.json({ isAdmin: false })
     }
 
     const address = await getWalletAddress(privyAuthToken)
-
-    // console.log('address', address)
 
     if (!(await isAdmin(address))) {
         return NextResponse.json({ isAdmin: false })
