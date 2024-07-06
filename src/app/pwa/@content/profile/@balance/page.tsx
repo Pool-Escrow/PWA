991ad306f9d8495c19df6a12392500e4046092e5
eldante <dan.tgw@gmail.com>
2024-07-07 01:07:42 +0800

    Add deposit button to open onRamp dialog.

--- src/app/pwa/@content/profile/@balance/page.tsx
+++ src/app/pwa/@content/profile/@balance/page.tsx
@@ -16,6 +16,9 @@ import { drop } from 'lodash'
 import { dropletAddress } from '@/types/droplet'
 import { wagmi } from '@/providers/configs'
 import { useTokenDecimals } from '@/lib/hooks/use-token-decimals'
+import OnRampDialog from '@/components/common/dialogs/onramp.dialog'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils/tailwind'
 
 export default function ProfileBalanceSection() {
     const { wallets } = useWallets()
@@ -27,15 +30,36 @@ export default function ProfileBalanceSection() {
 
     const decimals = BigInt(tokenBalanceData?.decimals ?? BigInt(18))
     const tokenBalance = ((tokenBalanceData?.value ?? BigInt(0)) / BigInt(10) ** decimals).toString()
+    const [openOnRampDialog, setOpenOnRampDialog] = useState(false)
+
     return (
         <Container>
             <SectionContent>
                 <div className='mx-2 flex flex-col justify-center'>
                     <h3 className='text-[11pt] font-semibold text-black'>Total Balance</h3>
-                    <h3 className='text-[36pt] font-bold text-[#2785EA]'>
+                    <h3 className='mb-4 text-[36pt] font-bold text-[#2785EA]'>
                         <span>{'$' + tokenBalance + ` `}</span>
                         <span className='text-[14pt]'>USDC</span>
                     </h3>
+                    <div className='flex w-full flex-row gap-x-4'>
+                        <Button
+                            className='mb-3 h-[40px] w-full rounded-lg bg-cta px-6 py-[11px] text-center text-base text-sm font-medium leading-normal text-white shadow-button active:shadow-button-push'
+                            onClick={(e: any) => {
+                                setOpenOnRampDialog(true)
+                            }}>
+                            Deposit
+                        </Button>
+                        <Button className='mb-3 h-[40px] w-full rounded-lg bg-cta px-6 py-[11px] text-center text-base text-sm font-medium leading-normal text-white shadow-button active:shadow-button-push'>
+                            Withdraw
+                        </Button>
+                    </div>
+
+                    <OnRampDialog
+                        open={openOnRampDialog}
+                        setOpen={setOpenOnRampDialog}
+                        balance={tokenBalanceData?.value}
+                        decimalPlaces={decimals}
+                    />
                 </div>
             </SectionContent>
         </Container>
