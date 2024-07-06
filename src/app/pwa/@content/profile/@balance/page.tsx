d7cd7615c994e53ce3b896b7e1d9d7519bf898d6
eldante <dan.tgw@gmail.com>
2024-07-06 17:29:08 +0800

    Added balance to profile page.

---
+++ src/app/pwa/@content/profile/@balance/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useAccount, useBalance } from 'wagmi'
+
+import { useUserStore } from '@/stores/profile.store'
+
+import { useRouter } from 'next/navigation'
+
+import Container from '@/components/common/other/container'
+import SectionContent from '@/components/claimable-prizes/section-content'
+
+import { useEffect, useState } from 'react'
+import { useWallets } from '@privy-io/react-auth'
+import { Address } from 'viem'
+import { drop } from 'lodash'
+import { dropletAddress } from '@/types/droplet'
+import { wagmi } from '@/providers/configs'
+import { useTokenDecimals } from '@/lib/hooks/use-token-decimals'
+
+export default function ProfileBalanceSection() {
+    const { wallets } = useWallets()
+
+    const { data: tokenBalanceData } = useBalance({
+        address: wallets[0]?.address as Address,
+        token: dropletAddress[wagmi.config.state.chainId as ChainId],
+    })
+
+    const decimals = BigInt(tokenBalanceData?.decimals ?? BigInt(18))
+    const tokenBalance = ((tokenBalanceData?.value ?? BigInt(0)) / BigInt(10) ** decimals).toString()
+    return (
+        <Container>
+            <SectionContent>
+                <h3 className='text-[11pt] font-semibold text-black'>Total Balance</h3>
+                <h3 className='text-[36pt] font-bold text-[#2785EA]'>
+                    <span>{'$' + tokenBalance + ` `}</span>
+                    <span className='text-[14pt]'>USDC</span>
+                </h3>
+            </SectionContent>
+        </Container>
+    )
+}
--- src/app/pwa/@content/profile/layout.tsx
+++ src/app/pwa/@content/profile/layout.tsx
@@ -1,7 +1,8 @@
-export default function ProfileLayout({ info, main }: LayoutWithSlots<'info' | 'main'>) {
+export default function ProfileLayout({ info, main, balance }: LayoutWithSlots<'info' | 'balance' | 'main'>) {
     return (
         <div className='flex flex-1 flex-col gap-6'>
             {info}
+            {balance}
             {main}
         </div>
     )
