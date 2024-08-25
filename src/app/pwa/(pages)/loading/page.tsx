4d9295798fd62a7949c0dcb30719c9ddd2bcb5ff
eldante <dan.tgw@gmail.com>
2024-08-25 17:58:38 +0800

    Added loading page.

---
+++ src/app/pwa/(pages)/loading/page.tsx
@@ -0,0 +1,8 @@
+/**
+ * @file src/app/my-pools/page.tsx
+ * @description This file initiates the participant pools page rendering.
+ */
+
+import LoadingIndicator from './_components/loading-indicator'
+
+export default LoadingIndicator
