ae62fc7e136e9d149aad4ba11db3fc0ee9f71b3f
eldante <dan.tgw@gmail.com>
2024-08-25 15:55:05 +0800

    Center text for not authorized.

--- src/app/pwa/(pages)/pool/new/page.tsx
+++ src/app/pwa/(pages)/pool/new/page.tsx
@@ -1,12 +1,13 @@
 import { getAuthStatus } from '@/app/pwa/_server/auth/auth.action'
 import CreatePoolForm from './create-pool-form'
+import { cn } from '@/lib/utils/tailwind'
 
 export default async function CreatePoolPage() {
     const authStatus = await getAuthStatus()
     const isAuthorized = authStatus.isAuthenticated && authStatus.isAdmin
 
     if (!isAuthorized) {
-        return <div>You are not authorized to create a pool.</div>
+        return <div className={cn('mt-4 w-full text-center')}>You are not authorized to create a pool.</div>
     }
 
     return <CreatePoolForm />
