3f02fb8f2a5175293ea8cc71dec3d4330f3605b5
eldante <dan.tgw@gmail.com>
2024-09-03 09:16:02 +0900

    Removed cn

--- src/app/pwa/(pages)/pool/new/page.tsx
+++ src/app/pwa/(pages)/pool/new/page.tsx
@@ -1,13 +1,12 @@
 import { getAuthStatus } from '@/app/pwa/_server/auth/auth.action'
 import CreatePoolForm from './create-pool-form'
-import { cn } from '@/lib/utils/tailwind'
 
 export default async function CreatePoolPage() {
     const authStatus = await getAuthStatus()
     const isAuthorized = authStatus.isAuthenticated && authStatus.isAdmin
 
     if (!isAuthorized) {
-        return <div className={cn('mt-4 w-full text-center')}>You are not authorized to create a pool.</div>
+        return <div className={'mt-4 w-full text-center'}>You are not authorized to create a pool.</div>
     }
 
     return <CreatePoolForm />
