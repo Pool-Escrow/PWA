55dd6d84ca21ec941e706d52993f355e17dbc021
eldante <dan.tgw@gmail.com>
2024-10-03 14:58:30 +0800

    Update profile circle color and font weight.

--- src/app/(pages)/pool/[pool-id]/_components/avatars.tsx
+++ src/app/(pages)/pool/[pool-id]/_components/avatars.tsx
@@ -18,14 +18,14 @@ const Avatars = ({ numPeople, className, avatarUrls }: AvatarsProps) => {
             {displayedAvatars.map((url, index) => (
                 <Avatar
                     key={index}
-                    className='size-10 rounded-full border-2 border-white dark:border-gray-800'
+                    className='size-10 rounded-full border-2 border-white bg-white'
                     aria-label='User Avatar'>
                     <AvatarImage alt={`Avatar ${index + 1}`} src={url} />
                     <AvatarFallback className='bg-[#d9d9d9]' />
                 </Avatar>
             ))}
             {remainingCount > 0 && (
-                <div className='z-10 flex size-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black'>
+                <div className='z-10 flex size-10 items-center justify-center rounded-full border-2 border-white bg-[#2989EC] text-center text-xs font-bold text-white'>
                     +{remainingCount}
                 </div>
             )}
