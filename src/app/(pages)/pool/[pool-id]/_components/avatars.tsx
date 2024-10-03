87b9c3c78c6a5c2ee4fc4c8d82d936298bf55acc
eldante <dan.tgw@gmail.com>
2024-10-03 15:14:03 +0800

    Fixed number formatting for larger numbers.

--- src/app/(pages)/pool/[pool-id]/_components/avatars.tsx
+++ src/app/(pages)/pool/[pool-id]/_components/avatars.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/app/_components/ui/avatar'
+import { formatNumberToMetric } from '@/app/_lib/utils/numbers'
 import { cn } from '@/lib/utils/tailwind'
 
 interface AvatarsProps {
@@ -12,6 +13,7 @@ interface AvatarsProps {
 const Avatars = ({ numPeople, className, avatarUrls }: AvatarsProps) => {
     const displayedAvatars = avatarUrls.slice(-4)
     const remainingCount = Math.max(0, (numPeople ?? avatarUrls.length) - 4)
+    const formattedCount = formatNumberToMetric(remainingCount)
 
     return (
         <div className={cn('z-10 flex -space-x-2 rtl:space-x-reverse', className)}>
@@ -26,7 +28,7 @@ const Avatars = ({ numPeople, className, avatarUrls }: AvatarsProps) => {
             ))}
             {remainingCount > 0 && (
                 <div className='z-10 flex size-10 items-center justify-center rounded-full border-2 border-white bg-[#2989EC] text-center text-xs font-bold text-white'>
-                    +{remainingCount}
+                    +{formattedCount}
                 </div>
             )}
         </div>
