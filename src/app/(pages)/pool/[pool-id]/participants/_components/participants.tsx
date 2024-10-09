d9a2dff24377bfd15c4989dd42b83f3e3b86b3a2
eldante <dan.tgw@gmail.com>
2024-10-09 15:32:02 +0800

    Added Tabs for Participants

--- src/app/(pages)/pool/[pool-id]/participants/_components/participants.tsx
+++ src/app/(pages)/pool/[pool-id]/participants/_components/participants.tsx
@@ -2,22 +2,27 @@
 
 import { useEffect, useState, useMemo } from 'react'
 import { useAppStore } from '@/app/_client/providers/app-store.provider'
-import Link from 'next/link'
-import { QrCodeIcon, SearchIcon } from 'lucide-react'
-import { Input } from '@/app/_components/ui/input'
 import ParticipantCard from './participantRow'
 import { useParticipants } from '@/hooks/use-participants'
 import SearchBar from './searchBar'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/app/_components/ui/tabs'
 
 interface PoolParticipantsProps {
     poolId: string
     isAdmin: boolean
 }
 
+enum ParticipantStatus {
+    Registered = 'registered',
+    CheckedIn = 'checkedIn',
+    Winners = 'winners',
+}
+
 const Participants = ({ poolId, isAdmin }: PoolParticipantsProps) => {
     const setTopBarTitle = useAppStore(state => state.setTopBarTitle)
     const [query, setQuery] = useState('')
     const { data: participants, isLoading, error } = useParticipants(poolId)
+    const [currentTab, setCurrentTab] = useState(ParticipantStatus.Registered)
 
     const filteredParticipants = useMemo(() => {
         return (
@@ -35,6 +40,10 @@ const Participants = ({ poolId, isAdmin }: PoolParticipantsProps) => {
         setQuery(e.target.value)
     }
 
+    const handleTabChange = (value: ParticipantStatus) => {
+        setCurrentTab(value)
+    }
+
     if (isLoading) return <div>Loading participants...</div>
     if (error) return <div>Error loading participants</div>
 
@@ -42,7 +51,27 @@ const Participants = ({ poolId, isAdmin }: PoolParticipantsProps) => {
         <div className='mx-auto max-w-md overflow-hidden rounded-lg bg-white'>
             <div className='p-4'>
                 <SearchBar query={query} onChange={handleChange} poolId={poolId} isAdmin={isAdmin} />
-                <ParticipantList participants={filteredParticipants} poolId={poolId} isAdmin={isAdmin} />
+                <Tabs
+                    defaultValue={ParticipantStatus.Registered}
+                    className='w-full'
+                    onValueChange={(value: string) => handleTabChange(value as ParticipantStatus)}>
+                    <TabsList className='z-10 flex w-full justify-start space-x-0 rounded-none bg-white p-0 md:space-x-8'>
+                        <TabsTrigger value={ParticipantStatus.Registered}>Registered</TabsTrigger>
+                        <TabsTrigger value={ParticipantStatus.CheckedIn}>Checked in</TabsTrigger>
+                        <TabsTrigger value={ParticipantStatus.Winners}>Winners</TabsTrigger>
+                        {/* <TabsTrigger value='refunded'>Refunded</TabsTrigger> */}
+                    </TabsList>
+                    <TabsContent value='registered'></TabsContent>
+                    <TabsContent value='checkedIn'></TabsContent>
+                    <TabsContent value='winners'></TabsContent>
+                    {/* <TabsContent value='refunded'>Refunded</TabsContent> */}
+                    <ParticipantList
+                        participants={filteredParticipants}
+                        poolId={poolId}
+                        isAdmin={isAdmin}
+                        tabValue={currentTab}
+                    />
+                </Tabs>
             </div>
         </div>
     )
@@ -52,10 +81,12 @@ const ParticipantList = ({
     participants,
     poolId,
     isAdmin,
+    tabValue,
 }: {
     participants: ReturnType<typeof useParticipants>['data']
     poolId: string
     isAdmin: boolean
+    tabValue: ParticipantStatus
 }) => (
     <>
         {participants && participants.length > 0 ? (
