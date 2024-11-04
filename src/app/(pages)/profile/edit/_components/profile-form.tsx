6b42791a1cedf2ab5f045afa729e8b3926a836aa
eldante <dan.tgw@gmail.com>
2024-11-04 11:54:54 +0800

    Edited the logic for setting bottom bar on profile.

--- src/app/(pages)/profile/edit/_components/profile-form.tsx
+++ src/app/(pages)/profile/edit/_components/profile-form.tsx
@@ -61,16 +61,16 @@ export default function ProfileForm({ userInfo }: ProfilePageProps) {
     const [state, formAction] = useFormState(validateProfileAction, initialState)
 
     useEffect(() => {
-        if (isPageTransitioning) return
-
-        setBottomBarContent(
-            <Button
-                type='submit'
-                form='profile-form'
-                className='mb-3 h-[46px] w-full rounded-[2rem] bg-cta px-6 py-[11px] text-center text-base font-semibold leading-normal text-white shadow-button active:shadow-button-push'>
-                Save
-            </Button>,
-        )
+        if (!isPageTransitioning) {
+            setBottomBarContent(
+                <Button
+                    type='submit'
+                    form='profile-form'
+                    className='mb-3 h-[46px] w-full rounded-[2rem] bg-cta px-6 py-[11px] text-center text-base font-semibold leading-normal text-white shadow-button active:shadow-button-push'>
+                    Save
+                </Button>,
+            )
+        }
 
         return () => {
             setBottomBarContent(null)
