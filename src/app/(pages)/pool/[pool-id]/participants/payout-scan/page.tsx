3483ae665f6c420a79695962b9b6b12860287539
eldante <dan.tgw@gmail.com>
2025-01-20 00:04:54 +0800

    Added incomplete payout-scan page.

--- src/app/(pages)/pool/[pool-id]/participants/payout-scan/page.tsx
+++ src/app/(pages)/pool/[pool-id]/participants/payout-scan/page.tsx
@@ -1,71 +1,184 @@
-import PageWrapper from '@/components/page-wrapper'
+'use client'
 
-export default function PayoutScanPage() {
-    return (
-        <PageWrapper topBarProps={{ title: 'Scan to Payout', backButton: true }}>
-            <h1>ScanQR</h1>
-        </PageWrapper>
-    )
+import * as React from 'react'
+import { useState, useEffect, useRef, useCallback } from 'react'
+
+import BackCircleButton from '@/components/back-circle-button'
+import { useParams } from 'next/navigation'
+import { QrCodeCheckInData } from '@/types/qr'
+import { usePoolCreationStore } from '@/app/_client/stores/pool-creation-store'
+import { useRouter } from 'next/navigation'
+
+const useCanvasContextOverride = () => {
+    useEffect(() => {
+        const originalGetContext = HTMLCanvasElement.prototype.getContext
+
+        const customGetContext = function (
+            this: HTMLCanvasElement,
+            contextId: string,
+            options?: any,
+        ): RenderingContext | null {
+            if (contextId === '2d') {
+                options = options || {}
+                options.willReadFrequently = true
+            }
+            return originalGetContext.call(this, contextId, options)
+        }
+
+        // @ts-expect-error ts(2322) - This is a temporary fix to enable willReadFrequently for 2d context
+        HTMLCanvasElement.prototype.getContext = customGetContext
+
+        // Cleanup when unmounting the component
+        return () => {
+            HTMLCanvasElement.prototype.getContext = originalGetContext
+        }
+    }, [])
 }
 
-// import Appbar from '@/components/appbar'
-// import Page from '@/components/page'
-// import Section from '@/components/section'
-// import router from 'next/router'
-// import React, { useEffect, useState } from 'react'
-// import { QrReader } from 'react-qr-reader'
+// Participant Check-in Preview
+export default function CheckInPage() {
+    const [result, setResult] = useState<string | null>(null)
+    const [error, setError] = useState<string | null>(null)
+    const [isScanning, setIsScanning] = useState(true)
+    const [timeLeft, setTimeLeft] = useState(20)
+    const [checkInStatus, setCheckInStatus] = useState<{ success: boolean; message: string } | null>(null)
+    const params = useParams()
+    const timerRef = useRef<NodeJS.Timeout | null>(null)
+    const isProcessing = useRef(false)
+    useCanvasContextOverride()
 
-// const ScanQR: React.FC = () => {
-// 	const [, setQRData] = useState<string>()
-// 	const [parentRoute, setParentRoute] = useState<string>()
+    const router = useRouter()
 
-// 	const handleScan = async (data: string | null) => {
-// 		if (data) {
-// 			setQRData(data)
-// 			console.log('data', data)
+    const { showToast } = usePoolCreationStore(state => ({
+        showToast: state.showToast,
+    }))
+    const handleDecode = async (decodedResult: string) => {
+        if (isProcessing.current) return
 
-// 			try {
-// 				const dataObj = JSON.parse(data)
-// 				console.log('data', data)
-// 				router.push(
-// 					`/admin/pool-id/${dataObj?.poolId}/participants/${dataObj?.address}`,
-// 				)
-// 			} catch (error) {
-// 				console.error(error)
-// 			}
-// 		}
-// 	}
+        try {
+            // Parse the QR code JSON data
+            const qrData: QrCodeCheckInData = JSON.parse(decodedResult)
 
-// 	useEffect(() => {
-// 		const paths = router?.asPath.split('/')
-// 		paths.pop() // Remove the last sub-route
-// 		setParentRoute(paths.join('/'))
-// 	}, [])
+            setResult(qrData.address)
+            setError(null)
+            stopScanning()
+            isProcessing.current = true
 
-// 	return (
-// 		<Page>
-// 			<Appbar backRoute={`${parentRoute}`} pageTitle='Scan to Payout' />
-// 			<Section>
-// 				<div className='h-full w-full relative flex flex-col'>
-// 					<QrReader
-// 						className='w-full h-full'
-// 						scanDelay={1000}
-// 						onResult={(result, error) => {
-// 							if (!!result) {
-// 								setQRData(result?.getText())
-// 								handleScan(result?.getText())
-// 							}
+            showToast({ type: 'info', message: 'Directing to user payout' })
+            router.push(`/pool/${qrData.poolId}/participants/${qrData.address}/payout`)
+        } catch (err) {
+            showToast({
+                type: 'error',
+                message: err instanceof Error ? err.message : 'Invalid QR code format',
+            })
+        } finally {
+            isProcessing.current = false
+        }
+    }
 
-// 							if (!!error) {
-// 								console.info(error)
-// 							}
-// 						}}
-// 						constraints={{ facingMode: 'environment' }}
-// 					/>
-// 				</div>
-// 			</Section>
-// 		</Page>
-// 	)
-// }
+    const handleError = (err: Error | string) => {
+        setError(typeof err === 'string' ? err : err.message)
+        setResult(null)
+    }
 
-// export default ScanQR
+    const startScanning = useCallback(() => {
+        setIsScanning(true)
+        setTimeLeft(20)
+    }, [])
+
+    const stopScanning = useCallback(() => {
+        setIsScanning(false)
+        if (timerRef.current) {
+            clearInterval(timerRef.current)
+        }
+    }, [])
+
+    useEffect(() => {
+        startScanning()
+    }, [])
+
+    useEffect(() => {
+        if (isScanning) {
+            timerRef.current = setInterval(() => {
+                setTimeLeft(prevTime => {
+                    if (prevTime <= 1) {
+                        stopScanning()
+                        return 0
+                    }
+                    return prevTime - 1
+                })
+            }, 1000)
+        }
+
+        return () => {
+            if (timerRef.current) {
+                clearInterval(timerRef.current)
+            }
+        }
+    }, [isScanning, stopScanning])
+
+    return (
+        // <PageWrapper topBarProps={{ title: 'Check-in', backButton: true }}>
+        //     <div className='container mx-auto max-w-2xl p-4'>
+        //         <h1 className='mb-6 text-center text-3xl font-bold'>Participant Check-in Preview</h1>
+
+        //         <Card className='mb-6'>
+        //             <CardHeader>
+        //                 <CardTitle>QR Scanner</CardTitle>
+        //             </CardHeader>
+        //             <CardContent>
+        //                 <QrScanner
+        //                     onDecode={handleDecode}
+        //                     onError={handleError}
+        //                     startButtonText={isScanning ? 'Scanning...' : 'Start Scanning'}
+        //                     stopButtonText='Stop'
+        //                 />
+        //                 {isScanning && <p className='mt-2 text-center'>Time left: {timeLeft} seconds</p>}
+        //             </CardContent>
+        //             <CardFooter>
+        //                 {result && (
+        //                     <div className='flex items-center text-green-600'>
+        //                         <CheckCircle2 className='mr-2 h-5 w-5' />
+        //                         <span>Result: {result}</span>
+        //                     </div>
+        //                 )}
+        //                 {error && (
+        //                     <div className='flex items-center text-red-600'>
+        //                         <AlertCircle className='mr-2 h-5 w-5' />
+        //                         <span>Error: {error}</span>
+        //                     </div>
+        //                 )}
+        //             </CardFooter>
+        //         </Card>
+        //     </div>
+        // </PageWrapper>
+        // <PageWrapper>hello</PageWrapper>
+        // <PageWrapper>
+        // {/* <QrScanner
+        //     onDecode={handleDecode}
+        //     onError={handleError}
+        //     startButtonText={isScanning ? 'Scanning...' : 'Start Scanning'}
+        //     stopButtonText='Stop'
+        // /> */}
+        // </PageWrapper>
+        <div className='absolute left-0 top-0 flex h-full w-full'>
+            <QrScanner
+                onDecode={handleDecode}
+                onError={handleError}
+                startButtonText={isScanning ? 'Scanning...' : 'Start Scanning'}
+                stopButtonText='Stop'
+            />
+            <header className='absolute top-4 w-full text-white'>
+                <nav className='grid h-24 grid-cols-[1fr_auto_1fr] items-center'>
+                    <div className='ml-6 w-6'>
+                        <BackCircleButton />
+                    </div>
+                    <div className='text-center'>
+                        <div className='w-full text-center text-sm font-semibold'>Manage Participants</div>
+                    </div>
+                    <div className='justify-self-end'></div>
+                </nav>
+            </header>
+        </div>
+    )
+}
