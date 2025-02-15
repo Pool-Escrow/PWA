32a57981b19d3701877530531eeb0f3b8eea47e1
jcruzfff <95213271+jcruzfff@users.noreply.github.com>
2025-02-15 01:15:11 -0800

    new login button component and placement. kept all existing login buttons but just made main page more easily accessible

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -5,15 +5,23 @@ import { Skeleton } from '@/app/_components/ui/skeleton'
 import { usePrivy } from '@privy-io/react-auth'
 import UserAvatar from './user-avatar'
 import { useAuth } from '@/app/_client/hooks/use-auth'
+import { usePathname } from 'next/navigation'
 
 export default function UserMenu() {
     const { login } = useAuth()
     const { ready, authenticated } = usePrivy()
+    const pathname = usePathname()
+    const isMainPage = pathname === '/pools' || pathname === '/'
 
     if (!ready) return <Skeleton className='h-[30px] w-[46px] px-[10px] py-[5px]' />
 
     if (ready && authenticated) {
         return <UserAvatar />
+    }
+
+    // Don't show the login button in the top bar if we're on the main page
+    if (isMainPage) {
+        return null
     }
 
     return (
