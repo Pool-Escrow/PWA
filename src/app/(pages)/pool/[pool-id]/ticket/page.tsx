124f351a213e8dbc4d5653285d0559aeef1258f1
eldante <dan.tgw@gmail.com>
2025-02-19 15:39:18 +0800

    Removed comment.

--- src/app/(pages)/pool/[pool-id]/ticket/page.tsx
+++ src/app/(pages)/pool/[pool-id]/ticket/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import QRCode from 'react-qr-code'
-import { usePoolDetails } from './_components/use-pool-details'
 import PageWrapper from '@/components/page-wrapper'
 import { useUserInfo } from '@/hooks/use-user-info'
+import QRCode from 'react-qr-code'
+import { usePoolDetails } from './_components/use-pool-details'
 
 const TicketPage = ({ params: { 'pool-id': poolId } }: { params: { 'pool-id': string } }) => {
     const { poolDetails } = usePoolDetails(poolId)
@@ -22,7 +22,6 @@ const TicketPage = ({ params: { 'pool-id': poolId } }: { params: { 'pool-id': st
                 <h2 className='mb-8 text-center text-lg text-black'>{poolDetails?.poolDetailFromSC?.[1]?.poolName}</h2>
                 <div className='flex w-full flex-1 flex-col items-center justify-center'>
                     {hasJoined ? (
-                        // eslint-disable-next-line tailwindcss/no-custom-classname
                         <div className='cardBackground flex w-full max-w-lg rounded-3xl bg-[#F6F6F6] p-12'>
                             <QRCode
                                 size={256}
