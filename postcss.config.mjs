7d3a01549a0648f338745bb39e460ce85181a5ba
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-06-23 19:19:42 +0700

    🔧 chore(postcss): update environment variable check for cssnano plugin

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 
-import { inProduction } from './src/app/_lib/utils/environment.mjs'
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
         // minify and purges unused css
-        ...(inProduction ? { cssnano: {} } : {}),
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
     },
 }
 
