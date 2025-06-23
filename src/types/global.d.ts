d288a7722696d95831ea1d330a18a22ebca24fc2
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-06-23 18:20:27 +0700

    🔧 chore(types): refine CompositeTypes type definition and add new global logging variables

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -6,6 +6,39 @@ declare global {
     }
 
     type ChainId = keyof typeof poolAddress
+
+    // eslint-disable-next-line no-var
+    var __serverConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __rpcConfigLogged: Record<number, boolean> | undefined
+    // eslint-disable-next-line no-var
+    var __transportConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __serverInitLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __contractAddressLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __explorerUrlLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __lastLoggedPath: string | undefined
+    // eslint-disable-next-line no-var
+    var __lastBalanceValidation: string | undefined
+    // eslint-disable-next-line no-var
+    var __lastPoolsBalanceValidation: string | undefined
+    // eslint-disable-next-line no-var
+    var __lastChainSelectorLog: string | undefined
+    // eslint-disable-next-line no-var
+    var __privyConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __supabaseClientConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __supabaseServerConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __wagmiEnvLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __wagmiConfigLogged: boolean | undefined
+    // eslint-disable-next-line no-var
+    var __lastTransactionStatus: string | undefined
 }
 
 export {}
