77fee01a355d2051825e5a9815a9f0f5a911ae16
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-08-06 13:32:50 +0200

    🗑️ chore(config): remove Tailwind CSS and TypeScript configuration files

--- config/rewrites.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-
-/** @type {import('next').NextConfig['rewrites']} */
-export const getRewriteRules = () =>
-    Promise.resolve([
-        { source: '/profile/new', destination: '/profile/edit?new' },
-        { source: '/', destination: '/pools' },
-    ])
