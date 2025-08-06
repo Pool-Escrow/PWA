1a262ee59a038fae08d0cf738b0292216196a3f7
Otto G <5030059+ottodevs@users.noreply.github.com>
2025-08-06 13:33:09 +0200

    🔧 chore(config): simplify PostCSS configuration by removing unused plugins

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,15 +1,5 @@
-// @ts-check
-
-import { inProduction } from './src/app/_lib/utils/environment.mjs'
-
-/** @type {import('postcss-load-config').Config} */
 const config = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        // minify and purges unused css
-        ...(inProduction ? { cssnano: {} } : {}),
-    },
+  plugins: ['@tailwindcss/postcss'],
 }
 
 export default config
